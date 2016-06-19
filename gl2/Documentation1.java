6fc782cc2a28c6625f819ae7289a45e745929f23
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-06-19 22:21:21 +0300

    gl2 cont

---
+++ gl2/Documentation1.java
@@ -0,0 +1,7 @@
+/**Comment for Class*/
+public class Documentation1{
+   /**Comment for variable*/
+   public int i;
+   /**Comment for method*/
+   public void f(){}
+}
---
+++ gl2/ShowProperties.java
@@ -0,0 +1,12 @@
+package gl2;
+
+import java.util.Date;
+
+public class ShowProperties{
+  public static void main(String[] args){
+     System.getProperties().list(System.out);
+     System.out.println(System.getProperty("user.name"));
+     System.out.println(System.getProperty("java.library.path"));
+  }
+
+}

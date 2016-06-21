ae4b690bed982d6ae94f02fe839d1a5f9040e7be
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-06-21 15:28:38 +0300

    Chapter 2 is done

---
+++ gl2/hw/Incrementable.java
@@ -0,0 +1,32 @@
+package gl2.hw;
+
+public class Incrementable{
+
+   static int i = 0;
+   
+
+   static void increment(){
+     i++;
+   }
+
+   public static void main(String[] args){
+   
+   increment();
+   increment();
+   increment();
+   System.out.println(i);
+   
+   Incrementable i1 = new Incrementable();
+   Incrementable i2 = new Incrementable();
+
+   i1.i = 5;
+   increment();
+
+   System.out.println(i2.i);
+
+
+   }
+
+
+}
+

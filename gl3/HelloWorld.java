bd8866a1e3c6da00a350ffd115f3f8537a6c7235
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-06-21 16:20:55 +0300

    smth

---
+++ gl3/HelloWorld.java
@@ -0,0 +1,20 @@
+package gl3;
+
+import static java.lang.System.*;
+
+public class HelloWorld{
+
+   public static void main(String[] args){
+      print(args);
+      out.println("Test static import");   
+   }
+
+   private static void print(String[] args){
+     System.out.println("Hello World");
+   }
+
+
+
+
+
+}

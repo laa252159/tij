70ceeadaff0c82b2f6873cf70e7e5797e173d3a8
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-06-28 17:50:44 +0300

    add Literals & LongHexOct

---
+++ gl3/hw/LongHexOct.java
@@ -0,0 +1,11 @@
+package gl3.hw;
+
+public class LongHexOct{
+
+   public static void main(String[] args){
+      long l1 = 0x2fff3;
+      long l2 = 033234;
+      System.out.println(Long.toBinaryString(l1));
+      System.out.println(Long.toBinaryString(l2));
+   }
+}

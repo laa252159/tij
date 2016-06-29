cd61fb66296c62a23c36e3ac8c83bf3bae2b058d
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-06-29 22:25:32 +0300

    ee

---
+++ gl3/hw/MaxVals.java
@@ -0,0 +1,13 @@
+package gl3.hw;
+
+import java.util.*;
+
+public class MaxVals{
+
+   public static void main(String[] args){
+      System.out.println("long: " + Long.MAX_VALUE);
+      System.out.println("double: " + Double.MAX_EXPONENT);
+   }
+
+
+}

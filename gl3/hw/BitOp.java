a8b80291806db51312c1f433286936f1c1be6bef
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-06-30 10:42:20 +0300

    smth

---
+++ gl3/hw/BitOp.java
@@ -0,0 +1,16 @@
+package gl3.hw;
+
+public class BitOp{
+
+   public static void main(String[] args){
+      int bin1 = 0x5;
+      int bin2 = 0xA;
+      System.out.println("bin1 " + Integer.toBinaryString(bin1));
+      System.out.println("bin2 " + Integer.toBinaryString(bin2));
+      System.out.println("AND " + Integer.toBinaryString(bin1 & bin2));   
+      System.out.println("OR " + Integer.toBinaryString(bin1 | bin2));   
+      System.out.println("XOR " + Integer.toBinaryString(bin1 ^ bin2));   
+      System.out.println("~ bin1" + Integer.toBinaryString(~bin1));   
+      System.out.println("~ bin2" + Integer.toBinaryString(~bin2));   
+   }
+}

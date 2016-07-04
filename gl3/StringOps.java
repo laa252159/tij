673c0e0ab7fb11169de0d1dcc60f21f914ce5720
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-07-04 22:01:58 +0300

    d

---
+++ gl3/StringOps.java
@@ -0,0 +1,17 @@
+package gl3;
+
+public class StringOps{
+   public static void main(String[] args){
+      int x = 0, y = 1, z = 2;
+      String s = "x, y, z ";
+      System.out.println(s + x + y + z);  
+      System.out.println(x + " " + s);
+      s += "(summed) =";
+      System.out.println(s + (x + y + z));  
+      System.out.println(" " + x);  
+         
+   
+   }
+}
+
+

bbd6f26a9654951894cce838a3a2c82acf5ce9c9
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-07-04 22:29:48 +0300

    s

---
+++ gl3/RoutingNums.java
@@ -0,0 +1,13 @@
+package gl3;
+
+
+public class RoutingNums{
+   public static void main(String[] args){
+      double above = 0.7, below = 0.4;
+      float fabove = 0.7f, fbelow = 0.4f;
+      System.out.println("Math.round(fabove): " + Math.round(above));
+      System.out.println("Math.round(below): " + Math.round(below));
+      System.out.println("Math.round(fabove): " + Math.round(fabove));
+      System.out.println("Mathe.round(fbelow): " + Math.round(fbelow));
+   }
+}

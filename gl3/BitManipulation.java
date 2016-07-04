db2b5ee0b31e7d0867414506d1076c10ca4f9d27
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-07-04 16:49:23 +0300

    sdf

--- gl3/BitManipulation.java
+++ gl3/BitManipulation.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class BitManipulation{
    
   public static void main(String[] argd){
-     Random rand = new Random();
+     Random rand = new Random(47);
      int i = rand.nextInt();
      int j = rand.nextInt();
      
@@ -25,16 +25,38 @@ public class BitManipulation{
      printBinaryInt("i & j", i & j);
      printBinaryInt("i | j", i | j);
      printBinaryInt("i ^ j", i ^ j);
-     printBinaryInt("i << 5", i << 5);
+     System.out.println();
+     printBinaryInt("-i", -i);
+     printBinaryInt("-i << 5", -i << 5);
+     printBinaryInt("-i >> 5", -i >> 5);
+     printBinaryInt("-i >> -5", (-i >> -5) >> 2);
+     printBinaryInt("(~i) >> 5", (~i) >> 5);
+     printBinaryInt("i >>> 5", i >>> 5);
+     printBinaryInt("(~i) >>> 5", (~i) >>> 5);
+
+     long l = rand.nextLong();
+     long m = rand.nextLong();
+     printBinaryLong("-1L", -1L);
+     printBinaryLong("+1L", +1L);
+     long ll = 9223372036854775807L;
+     printBinaryLong("max pos", ll);
+     long lln = -9223372036854775808L;
+     printBinaryLong("mac neg", lln);
+     printBinaryLong("l", l);
+     printBinaryLong("~l", ~l);
+     printBinaryLong("m", m);
+     printBinaryLong("l & m", l & m);
+     printBinaryLong("l | m", l | m);
+     printBinaryLong("l ^ m", l ^ m);
+     System.out.println();
+     printBinaryLong("l", l);
+     printBinaryLong("l << 5", l << 5);
 
 
-
-          
-          
-     
-     
-     
-  
+     printBinaryLong("l >> 5", l >> 5);
+     printBinaryLong("(~l) >> 5", (~l) >> 5);
+     printBinaryLong("l >>> 5", l >>> 5);
+     printBinaryLong("(~l) >>> 5", (~l) >>> 5);
   }  
 
   static void printBinaryInt(String s, int i){
@@ -44,6 +66,6 @@ public class BitManipulation{
 
   static void printBinaryLong(String s, long l){
      System.out.println(s + ", long: " + l + ", binary: \n " + 
-     	Integer.toBinaryString(l));
+     	Long.toBinaryString(l));
   }
 }

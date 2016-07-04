34ad1cc4d5b9bb125d5df9d745820e8885f7b042
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-07-04 17:28:42 +0300

    git LR binary

--- gl3/hw/BinShiftLR.java
+++ gl3/hw/BinShiftLR.java
@@ -1,16 +1,18 @@
 package gl3.hw;
 
-public class BinShift{
+public class BinShiftLR{
    public static void main(String[] args){
       int num = 123456;
       toBS(num);
-      for(int i = 0 ; i < 16; i++){
-         num >>= 1;
+      num <<= 1;
+      toBS(num);
+      for(int i = 0 ; i < 17; i++){
+         num >>>= 1;
          toBS(num);
       } 
    }
 
    static void toBS(int num){
-      System.out.println("num :\t" + num + " binary : \t" + Integer.toBinaryString(num));
+      System.out.println("num :\t" + num + " bit count: \t " + Integer.bitCount(num) +" binary : \t" + Integer.toBinaryString(num));
    }
 }

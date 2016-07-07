dbc482e4fbc314e7d6a0f78bde9fd82552d59445
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-07-07 07:48:50 +0300

    d

---
+++ gl4/hw/RandInt.java
@@ -0,0 +1,16 @@
+package gl4.hw;
+
+import java.util.*;
+
+public class RandInt{
+    static int prev = 0;
+    public static void main(String[] args){
+        Random rand = new Random();
+	for(int i = 0; i < 25; i++){
+	   int val = rand.nextInt();
+	   System.out.println("Num : " + (i+1) + " value: " + val);
+	   if (i > 0) 
+	      System.out.println(val > prev ? "> than prev" : val < prev ? "< than prev" : "equals");
+	}
+    }
+}

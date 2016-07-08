03f3e63f12dc7bd9e90b6adf80ed15fc9ed9859f
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-07-08 11:04:09 +0300

    dd

---
+++ gl4/hw/NotDivNums.java
@@ -0,0 +1,22 @@
+package gl4.hw;
+
+public class NotDivNums{
+
+   public static void main(String[] args){
+      int num = 0;
+      for(int i = 1; i < 5000 ; i++){
+         int counter = 2; 
+         for(int j = 1; j <=i; j++){
+           if(i % j == 0){
+	      counter--;
+	   } 	 
+	 }
+	 if(counter == 0){
+	    System.out.println(i);
+	    num++;
+	 }
+      }
+      System.out.println(num);
+   }
+}
+

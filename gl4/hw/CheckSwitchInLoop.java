da9e6d620cf95e718c969f403857000010c6a8b2
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-07-14 00:28:27 +0300

    d

---
+++ gl4/hw/CheckSwitchInLoop.java
@@ -0,0 +1,17 @@
+package gl4.hw;
+
+public class CheckSwitchInLoop{
+
+   public static void main(String[] args){
+      for(int i = 0; i < 3; i++){
+          switch(){
+	  
+	  }
+ 
+      
+      }
+
+
+   }
+
+}

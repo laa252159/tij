9bc1b8d0ec381d5450da68144f49759a5aa32649
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-07-19 13:12:36 +0300

    s

---
+++ gl5/hw/InitNullStr.java
@@ -0,0 +1,10 @@
+package gl5.hw;
+
+public class InitNullStr{
+   String s;
+
+   public static void main(String[] args){
+      InitNullStr i = new InitNullStr();
+      System.out.println("value is: " + i.s);
+   }
+}

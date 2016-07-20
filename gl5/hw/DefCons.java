ca09f1727b88537f965c8d6458643496963f00f6
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-07-20 21:17:19 +0300

    s

---
+++ gl5/hw/DefCons.java
@@ -0,0 +1,17 @@
+package gl5.hw;
+
+class Smth{
+   Smth(){
+      System.out.println("!!!");
+   }
+   Smth(String str){
+      System.out.println("!!!" + str);
+   }
+}
+
+public class DefCons{
+   public static void main(String[] args){
+      Smth smth = new Smth(args[0]);
+   }
+
+}

243d1dcb92b10b77fab2aefa3c33f87ed196ae52
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-07-23 18:09:12 +0300

    d

---
+++ gl5/hw/Finalize.java
@@ -0,0 +1,16 @@
+package gl5.hw;
+
+public class Finalize{
+   
+   public void finalize(){
+      System.out.println("finalize event");
+   }
+   
+   public static void main(String[] args){
+      new Finalize();
+      System.gc();
+      while(true){
+      }
+   }
+
+}

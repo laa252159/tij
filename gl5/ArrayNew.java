72319c4d1eef0d29b0a14fab9c38d5f738e08868
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-08-03 11:28:54 +0300

    ee

--- gl5/ArrayNew.java
+++ gl5/ArrayNew.java
@@ -6,11 +6,17 @@ public class ArrayNew{
    public static void main(String[] args){
       int[] a;
       int[] b; 
+      char[] c; 
+      char[] d; 
       b = new int[]{1, 22, 44, 44, 55};
+      d = new char[]{0, 1, 2, 3, 4};
       Random rand = new Random(47);
+      c = new char[rand.nextInt(20)];
       a = new int[rand.nextInt(20)];
       System.out.println("Length a = " + a.length);
       System.out.println(Arrays.toString(a));
       System.out.println(Arrays.toString(b));
+      System.out.println(Arrays.toString(c));
+      System.out.println(Arrays.toString(d));
    }
 }

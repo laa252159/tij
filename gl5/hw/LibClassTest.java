e9f2d7e9ad66b5a1794e10777d09bbc2ce72b40e
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-08-12 10:24:18 +0300

    s

---
+++ gl5/hw/LibClassTest.java
@@ -0,0 +1,11 @@
+package gl5.hw;
+
+import gl5.hw.pack1.*;
+
+public class LibClassTest{
+   public static void main(String[] args){
+      LibClass lc = new LibClass();
+      lc.test();
+   }
+}
+
---
+++ gl5/hw/pack1/LibClass.java
@@ -0,0 +1,7 @@
+package gl5.hw.pack1;
+
+public class LibClass{
+   public void test(){
+      System.out.println("Yaahooo");   
+   }
+}

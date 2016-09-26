9b9cd3751078d748d62560e360d2d0414c6d8937
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-09-26 21:12:08 +0300

    ss

---
+++ gl6/hw/access/Foreign.java
@@ -0,0 +1,9 @@
+package gl6.hw.access;
+
+import gl6.hw.access.local.*;
+
+public class Foreign{
+   public static void main(String[] args){
+      PackageClass pc = new PackageClass();
+   }
+}
---
+++ gl6/hw/access/local/PackageClass.java
@@ -0,0 +1,7 @@
+package gl6.hw.access.local;
+
+public class PackageClass{
+   public PackageClass(){
+      System.out.println("Making class in a package");
+   }
+}

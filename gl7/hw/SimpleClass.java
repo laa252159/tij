66546e1d2fbfcc62eec6aedb406eb272e256086e
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2016-09-29 17:00:02 +0300

    dd

---
+++ gl7/hw/SimpleClass.java
@@ -0,0 +1,7 @@
+package gl7.hw;
+
+class SimpleClass{
+  public String toString(){
+     return "simple";
+  }
+}
---
+++ gl7/hw/TestSimpleClass.java
@@ -0,0 +1,20 @@
+package gl7.hw;
+
+class TestSimpleClass{
+   private SimpleClass sc;
+   
+   public SimpleClass getSc(){
+      if(sc == null){
+         sc = new SimpleClass();
+      }
+      return sc;
+   }
+
+   public static void main(String[] args){
+      System.out.println(new TestSimpleClass().getSc());      
+   }
+
+   public String toString(){
+      return "test";
+   }
+}

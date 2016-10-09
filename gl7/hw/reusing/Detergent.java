e30c2a6e2040d0e7c829622ca720636a645e223e
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-10-09 10:20:02 +0300

    ss

--- gl7/hw/reusing/Detergent.java
+++ gl7/hw/reusing/Detergent.java
@@ -1,13 +1,46 @@
 package gl7.hw.reusing;
 
-public class Detergent extends Cleanser{
+public class Detergent{
    
+   Cleanser c = new Cleanser();
+
    public void scrub(){
-      append(" Detergent.scrub()");
-      super.scrub();
+      c.scrub();
    }
    
-   public void foam(){
-      append(" foam()");
+   public void delute(){
+      c.delute();
+   }
+
+
+   public static void main(String[] args){
+      Cleanser x = new Cleanser();
+      x.delute();
+      x.scrub();
+      System.out.println(x);
+   }
+}
+
+class Cleanser{
+   private String s = "Cleanser";
+   
+   public void append(String a){
+      s += a;
+   }
+
+   public void delute(){
+      append(" dilute()");
+   }
+
+   public void apply(){
+      append(" apply()");
+   }
+
+   public void scrub(){
+      append(" scrub()");
+   }
+
+   public String toString(){
+      return s;
    }
 }

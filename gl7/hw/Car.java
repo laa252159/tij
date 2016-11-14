c50e296e84665293886d71c425cf7c7b2dff3832
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-11-14 22:56:54 +0300

    d

--- gl7/hw/Car.java
+++ gl7/hw/Car.java
@@ -4,6 +4,9 @@ class Engine{
    public void start(){}
    public void rev(){}
    public void stop(){}
+   public void engine(){
+      System.out.println("Engine");
+   }
 }
 
 class Wheel{
@@ -15,13 +18,13 @@ class Window{
    public void rolldown(){}
 }
 
-class Door(){
+class Door{
    public Window window = new Window();
    public void open(){}
    public void close(){}
 }
 
-public class Car(){
+public class Car{
    public Engine engine = new Engine();
    public Wheel[] wheels = new Wheel[4];
    public Door
@@ -31,12 +34,14 @@ public class Car(){
    public Car(){
       for(int i = 0; i < 4; i++){
          wheels[i] = new Wheel();
+      }
    }
 
       public static void main(String[] args){
          Car car = new Car();
          car.left.window.rollup();
          car.wheels[0].inflate(72);
+	 car.engine.engine();
       }
-   }
 }
+

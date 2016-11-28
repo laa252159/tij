c77332f20bf959819022983dc4d01af6d6107590
Alexander Lobov <Aleksandr_Lobov@epam.com>
2016-11-28 20:54:48 +0300

    Final

---
+++ gl7/hw/Final.java
@@ -0,0 +1,30 @@
+package gl7.hw;
+
+class Table{
+   final int num;
+
+   public Table(int num){
+      this.num = num;
+   }
+
+}
+
+public class Final{
+   final int i;
+   final Table table;
+
+
+   public Final(int i){
+      this.i = i;
+      this.table = new Table(i);
+   }
+
+   public String toString(){
+      return "i = " + i + " table.num = " + table.num; 
+   }
+
+   public static void main(String[] args){
+      Final f = new Final(10);
+      System.out.println(f);
+   }
+}

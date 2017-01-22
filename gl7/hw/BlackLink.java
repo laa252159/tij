c1ac0facce01af5163e3b9eff0a947645789578a
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-01-22 22:19:45 +0400

    ss

---
+++ gl7/hw/BlackLink.java
@@ -0,0 +1,20 @@
+package gl7.hw;
+
+public class BlackLink{
+   final String str;
+
+   public BlackLink(){
+      str = null;
+   }
+
+   public BlackLink(String str){
+      this.str = str;
+   }
+
+   public static void main(String[] args){
+      BlackLink bl = new BlackLink();
+      BlackLink bl2 = new BlackLink("S");
+      System.out.println("That's all");
+   }
+
+}

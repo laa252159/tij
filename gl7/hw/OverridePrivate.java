5e2f85da9f47709905d924f18fae30554a0e771a
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-01-25 22:38:50 +0400

    ClassLoading

--- gl7/hw/OverridePrivate.java
+++ gl7/hw/OverridePrivate.java
@@ -6,13 +6,22 @@ public class OverridePrivate extends BaseClass{
         System.out.println("from OverridePrivate");
      }
     public static void main(String[] args){
-       BaseClass o = new OverridePrivate();
+       BaseClass o1 = new BaseClass();
+       BaseClass o2 = new OverridePrivate();
+       o1.f();
+       ((OverridePrivate)o2).f();
     }
+    final void f(){
+       System.out.println("Override");
+    }
+
 }
 
 class BaseClass{
    private void dot(){
       System.out.println("from base class");
    }
-
+   void f(){
+      System.out.println("Base");
+   }
 }

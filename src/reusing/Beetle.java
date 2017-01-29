20adaee2e62ecb0b0c9bc2f42c130f3a3fb66e24
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-01-29 14:17:01 +0400

    add

--- src/reusing/Beetle.java
+++ src/reusing/Beetle.java
@@ -1,18 +1,18 @@
 //: reusing/Beetle.java
 // The full process of initialization.
-import static net.mindview.util.Print.*;
+//import static net.mindview.util.Print.*;
 
 class Insect {
   private int i = 9;
   protected int j;
   Insect() {
-    print("i = " + i + ", j = " + j);
+    System.out.println("i = " + i + ", j = " + j);
     j = 39;
   }
   private static int x1 =
     printInit("static Insect.x1 initialized");
   static int printInit(String s) {
-    print(s);
+    System.out.println(s);
     return 47;
   }
 }
@@ -20,16 +20,32 @@ class Insect {
 public class Beetle extends Insect {
   private int k = printInit("Beetle.k initialized");
   public Beetle() {
-    print("k = " + k);
-    print("j = " + j);
+    System.out.println("k = " + k);
+    System.out.println("j = " + j);
   }
   private static int x2 =
     printInit("static Beetle.x2 initialized");
   public static void main(String[] args) {
-    print("Beetle constructor");
+    System.out.println("Beetle constructor");
     Beetle b = new Beetle();
+    System.out.println("BeetleSecond constructor");
+    BeetleSecond bs = new BeetleSecond();
   }
-} /* Output:
+}
+
+class BeetleSecond extends Beetle{
+
+  private int s = printInit("Beetle.k initialized");
+  public BeetleSecond() {
+    System.out.println("s = " + s);
+    System.out.println("j = " + j);
+  }
+  private static int x2 =
+    printInit("static BeetleSecond.x2 initialized");
+}
+
+
+/* Output:
 static Insect.x1 initialized
 static Beetle.x2 initialized
 Beetle constructor

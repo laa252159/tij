3a983401276234ddc0615c6f550cd9fc74c27dd2
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-02-19 20:15:46 +0400

    page 258 ex 16

--- src/polymorphism/PolyConstructors.java
+++ src/polymorphism/PolyConstructors.java
@@ -5,7 +5,7 @@ package polymorphism;
 import static net.mindview.util.Print.*;
 
 class Glyph {
-  void draw() { print("Glyph.draw()"); }
+  void draw() { print("Glyph.draw()" + " DRAW Glyph"); }
   Glyph() {
     print("Glyph() before draw()");
     draw();
@@ -20,13 +20,25 @@ class RoundGlyph extends Glyph {
     print("RoundGlyph.RoundGlyph(), radius = " + radius);
   }
   void draw() {
-    print("RoundGlyph.draw(), radius = " + radius);
+    print("RoundGlyph.draw(), radius = " + radius + " DRAW RoundGlyph");
+  }
+}	
+
+class RectangularGlyph extends RoundGlyph {
+  private int radius = 4;
+  RectangularGlyph(int r) {
+    super(1);
+    radius = r;
+    print("RectangularGlyph.RoundGlyph(), radius = " + radius);
+  }
+  void draw() {
+    print("RectangularGlyph.draw(), radius = " + radius + " DRAW RectangularGlyph");
   }
 }	
 
 public class PolyConstructors {
   public static void main(String[] args) {
-    new RoundGlyph(5);
+    new RectangularGlyph(5);
   }
 } /* Output:
 Glyph() before draw()

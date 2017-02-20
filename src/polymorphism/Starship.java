2c47b39d42b2dffbfdb4d9bd2f60d2243ecca834
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-02-20 23:04:30 +0400

    page 260 ex 17

---
+++ src/polymorphism/Starship.java
@@ -0,0 +1,34 @@
+package polymorphism;
+// Dynamically changing the behavior of an object
+// via composition (the "State" design pattern).
+import static net.mindview.util.Print.*;
+
+class AlertStatus {
+  public void status() {}
+}
+
+class Fire extends AlertStatus {
+  public void status() { print("Fire"); }
+}
+
+class Meteor extends AlertStatus {
+  public void status() { print("Meteor"); }
+}
+
+class Stage {
+  private AlertStatus status = new Fire();
+  public void change() { status = new Meteor(); }
+  public void act() { status.status(); }
+}
+
+public class Starship {
+  public static void main(String[] args) {
+    Stage stage = new Stage();
+    stage.act();
+    stage.change();
+    stage.act();
+  }
+} /* Output:
+HappyActor
+SadActor
+*///:~

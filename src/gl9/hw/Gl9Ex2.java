70bcc4310d9b713435b85eaee12c1165c17ec9f3
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-02-28 22:31:54 +0400

    dd

---
+++ src/gl9/hw/Gl9Ex2.java
@@ -0,0 +1,12 @@
+package gl9.hw;
+
+public class Gl9Ex2{
+   public static void main(String[] args){
+      System.out.println(Numbers.ONE);
+      // Numbers.THREE = 4; Final!!! Doesn't compile 
+   }  
+}
+
+interface Numbers{
+   int ONE = 1, TWO = 2, THREE = 3;
+}

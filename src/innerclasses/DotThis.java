b1598d30b3bab3c0aa8bca69799fe4a119fb2476
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-03-24 06:56:04 +0400

    dd

--- src/innerclasses/DotThis.java
+++ src/innerclasses/DotThis.java
@@ -1,10 +1,13 @@
 //: innerclasses/DotThis.java
 // Qualifying access to the outer-class object.
+package innerclasses;
 
 public class DotThis {
+  String str;
   void f() { System.out.println("DotThis.f()"); }
   public class Inner {
     public DotThis outer() {
+      DotThis.this.str = "done";
       return DotThis.this;
       // A plain "this" would be Inner's "this"
     }
@@ -12,8 +15,10 @@ public class DotThis {
   public Inner inner() { return new Inner(); }
   public static void main(String[] args) {
     DotThis dt = new DotThis();
+    System.out.println(dt.str);
     DotThis.Inner dti = dt.inner();
     dti.outer().f();
+    System.out.println(dt.str);
   }
 } /* Output:
 DotThis.f()

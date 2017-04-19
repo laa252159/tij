ff54c7b4436354713c47fd9df907429ed0e7143e
sanix <sanix>
2017-04-19 18:52:18 +0400

    gl10 ex14

--- src/interfaces/Factories.java
+++ src/interfaces/Factories.java
@@ -13,27 +13,25 @@ interface ServiceFactory {
 class Implementation1 implements Service {
   Implementation1() {} // Package access
   public void method1() {print("Implementation1 method1");}
-  public void method2() {print("Implementation1 method2");}
-}	
-
-class Implementation1Factory implements ServiceFactory {
-  public Service getService() {
-    return new Implementation1();
-  }
+  public void method2() {print("Implementation1 method2");}	
+  
+  static ServiceFactory f1 = new ServiceFactory()  {
+    public Service getService() {
+      return new Implementation1();
+    }
+  };
 }
 
 class Implementation2 implements Service {
   Implementation2() {} // Package access
   public void method1() {print("Implementation2 method1");}
   public void method2() {print("Implementation2 method2");}
-}
-
-class Implementation2Factory implements ServiceFactory {
-  public Service getService() {
-    return new Implementation2();
-  }
-}	
-
+  static ServiceFactory f2 = new ServiceFactory(){
+     public Service getService(){
+       return new Implementation2(); 
+     }	  
+   };
+ }
 public class Factories {
   public static void serviceConsumer(ServiceFactory fact) {
     Service s = fact.getService();
@@ -41,9 +39,9 @@ public class Factories {
     s.method2();
   }
   public static void main(String[] args) {
-    serviceConsumer(new Implementation1Factory());
+    serviceConsumer(Implementation1.f1);
     // Implementations are completely interchangeable:
-    serviceConsumer(new Implementation2Factory());
+    serviceConsumer(Implementation2.f2);
   }
 } /* Output:
 Implementation1 method1

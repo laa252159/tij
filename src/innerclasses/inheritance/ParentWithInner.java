f85cbc9ee58f44227bf1772a93fc6e32aec65eae
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2017-05-31 12:19:04 +0400

    ch10 ex24

--- src/innerclasses/inheritance/ParentWithInner.java
+++ src/innerclasses/inheritance/ParentWithInner.java
@@ -1,35 +1,38 @@
-package innerclasses.inherit;
-
 public class ParentWithInner {
-   private String commonField;
+	private String commonField;
 
-   public class InnerInParent {
-      private int i;
-      private String s;
-      public InnerInParent(int i, String s, String commonF){
-         commonField = commonF;
-	 this.i = i;
-	 this.s = s;
-      }
+	public ParentWithInner(String commonField) {
+		this.commonField = commonField;
+	}
 
-      protected String toSting(){
-         return "i = " + i + " s = " + s + " Common = " + commonField;
-      }
-   }
+	public class InnerInParent {
+		private int i;
+		private String s;
 
-   public static void main(String[] args) {
-      ParentWithInner parent = new ParentWithInner();
-      InnerInParent inner = new InnerInParent(0, "**");
-      ChildWithInner.InnerInChild inner = (new ChildWithInner()).new InnerInChild(inner);     
-      System.out.println(inner);
-   }
+		public InnerInParent() {
+		}
+
+		public InnerInParent(int i, String s, String commonF) {
+			commonField = commonF;
+			this.i = i;
+			this.s = s;
+		}
+
+		public String toString() {
+			return "i = " + i + " s = " + s + " Common = " + commonField;
+		}
+	}
+
+	public static void main(String[] args) {
+		ParentWithInner parent = new ParentWithInner("***");
+		Child c = new Child(parent);
+		System.out.println(c);
+	}
 }
 
-class ChildWithInner {
-   public class InnerInChild extends ParentWithInner.InnerInParent {
-      public InnerInChild(InerIn Parent parent) {
-	 parent.this();
-	 super(7, 007, "!Common2!");
-      }   
-   }
+class Child extends ParentWithInner.InnerInParent {
+	Child(ParentWithInner p) {
+		p.super();
+	}
+
 }

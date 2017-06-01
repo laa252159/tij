c19638b092a690126d115094c90c4c17006e0c92
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2017-06-01 10:27:19 +0400

    Ch11 ex2

--- src/holding/SimpleCollection.java
+++ src/holding/SimpleCollection.java
@@ -1,9 +1,11 @@
+package holding;
+
 //: holding/SimpleCollection.java
 import java.util.*;
 
 public class SimpleCollection {
   public static void main(String[] args) {
-    Collection<Integer> c = new ArrayList<Integer>();
+    Collection<Integer> c = new HashSet<Integer>();
     for(int i = 0; i < 10; i++)
       c.add(i); // Autoboxing
     for(Integer i : c)

73b827106aa250d681d30a0914b6bad62878be15
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2017-06-27 19:04:11 +0400

    fix packages

--- src/gl11/hw/Ex15.java
+++ src/gl11/hw/Ex15.java
@@ -1,29 +1,27 @@
 package gl11.hw;
 
 import net.mindview.util.Stack;
+
 import java.util.*;
 
 public class Ex15 {
-   public static final String str = "+U+n+c---+e+r+t---+a-+i-+n+t+y---+-+r+u--+l+e+s---";
-   public static void main(String[] args){
-     char[] chars = str.toCharArray();
-     Stack<Character> stack = new Stack<Character>();
-     Iterator<Character> iter = new LinkedList(Arrays.asList(chars)).iterator();
-     while(iter.hasNext()){
-        process(iter, stack);
-     }
-     StringBuilder sb = new StringBuilder();
-     while(!stack.empty()){
-        sb.append(stack.pop());
-     }
-     System.out.println(sb);
-   }
+	public static final String str = "+U+n+c---+e+r+t---+a-+i-+n+t+y---+-+r+u--+l+e+s---";
+	private final static Stack<Character> stack = new Stack<Character>();
 
-   public static void process(Iterator<Character> iter, Stack<Character> stack){
-       char c = iter.next();
-       switch(c){
-          case '+' : stack.push(iter.next()); break; 
-	      case '-' : stack.pop(); break;
-       }
-   }
+	private static void evaluate(String expr) {
+		char data[] = expr.toCharArray();
+		for (int i = 0; i < data.length; )
+			switch (data[i++]) {
+			case '+':
+				stack.push(data[i++]);
+				break;
+			case '-':
+				System.out.print(stack.pop());
+			}
+	}
+
+	public static void main(String[] args) {
+		evaluate("+U+n+c---+e+r+t---+a-+i-+n+t+y---+ -+r+u--+l+e+s---");
+	}
+
 }

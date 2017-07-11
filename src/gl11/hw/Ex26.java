5313453f0c1ca599f9659744f3158b3aa7a1d81a
Aleksandr Lobov <Aleksandr_Lobov@epam.com>
2017-07-11 13:15:33 +0400

    Gl11 Ex 25 Ex 26

---
+++ src/gl11/hw/Ex26.java
@@ -0,0 +1,47 @@
+package gl11.hw;
+
+import net.mindview.util.TextFile;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Ex26 {
+
+	static Map<String, ArrayList<Integer>> map = new HashMap<String, ArrayList<Integer>>();
+	static Map<Integer, String> rMap = new TreeMap<Integer, String>();
+
+	public static void main(String[] args) {
+		List<String> words = new TextFile("src/holding/SetOperations.java", "\\W+");
+		int counter = 0;
+		for (String word : words) {
+			List<Integer> freq = map.get(word);
+			if (freq == null) {
+				map.put(word, new ArrayList<Integer>(counter));
+			} else {
+				List<Integer> list = map.get(word);
+				list.add(counter);
+			}
+			counter++;
+		}
+		List<String> wSet = new ArrayList(map.keySet());
+		Collections.sort(wSet, String.CASE_INSENSITIVE_ORDER);
+		for (String w : wSet) {
+			for (Integer i : map.get(w)) {
+				rMap.put(i, w);  
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for(String str : rMap.values()){
+			sb.append(str);
+			sb.append("\t");
+		}
+		System.out.println(sb);
+	}
+	
+} 
+

a5b90932e06b6ef1cb9e084947f9f53823e9c494
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-07-24 21:23:50 +0400

    Ex32 gl11

--- src/holding/NonCollectionSequence.java
+++ src/holding/NonCollectionSequence.java
@@ -1,4 +1,4 @@
-//: holding/NonCollectionSequence.java
+package holding;//: holding/NonCollectionSequence.java
 import typeinfo.pets.*;
 import java.util.*;
 
@@ -19,9 +19,37 @@ public class NonCollectionSequence extends PetSequence {
       }
     };
   }
+
+  public Iterable<Pet> reversed() {
+    return new Iterable<Pet>() {
+      public Iterator<Pet> iterator() {
+        return new Iterator<Pet>() {
+          int current = pets.length - 1;
+          public boolean hasNext() { return current > -1; }
+          public Pet next() { return pets[current--]; }
+          public void remove() { // Not implemented
+            throw new UnsupportedOperationException();
+          }
+        };
+      }
+    };
+  }
+
+  public Iterable<Pet> randomized() {
+    return new Iterable<Pet>() {
+      public Iterator<Pet> iterator() {
+        List<Pet> shuffled =
+                new ArrayList<Pet>(Arrays.asList(pets));
+        Collections.shuffle(shuffled, new Random(47));
+        return shuffled.iterator();
+      }
+    };
+  }
   public static void main(String[] args) {
     NonCollectionSequence nc = new NonCollectionSequence();
     InterfaceVsIterator.display(nc.iterator());
+    InterfaceVsIterator.display(nc.reversed().iterator());
+    InterfaceVsIterator.display(nc.randomized().iterator());
   }
 } /* Output:
 0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx

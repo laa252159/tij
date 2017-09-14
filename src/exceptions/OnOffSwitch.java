11190656c2a9b4c811454faec39b9c2d8803011f
laa252159@gmail.com <Pyfybz!(85>
2017-09-14 18:18:10 +0400

    Ch12 Ex14

--- src/exceptions/OnOffSwitch.java
+++ src/exceptions/OnOffSwitch.java
@@ -5,7 +5,7 @@ public class OnOffSwitch {
   private static Switch sw = new Switch();
   public static void f()
   throws OnOffException1,OnOffException2 {
-    throw new OnOffException1();
+    throw new RuntimeException();
   }
   public static void main(String[] args) {
     try {

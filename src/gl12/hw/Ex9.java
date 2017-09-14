235ef6c5faa7aa6f038611803f513cdbed05b590
laa252159@gmail.com <Pyfybz!(85>
2017-09-14 18:15:36 +0400

    Ch12 Ex13

--- src/gl12/hw/Ex9.java
+++ src/gl12/hw/Ex9.java
@@ -1,17 +1,25 @@
 package gl12.hw;
 
+import java.util.Date;
 import java.util.Random;
 
 public class Ex9 {
     public static void main(String[] args) {
         Random random = new Random();
+        Exception exception = null;
         int var;
-            for (int i = 0 ; i < 20 ; i++){
+            for (int i = 0 ; i < 4 ; i++){
                 try {
-                    var = random.nextInt(3);
+                    var = random.nextInt(4);
                     generate(var);
                 } catch (Exception e){
-                    System.out.println(e);
+//                    System.out.println(e);
+                    exception = e;
+                } finally {
+                    System.out.println("FINALLY DONE Exception: " + exception);
+                    if (exception instanceof NullPointerException){
+                        System.out.println("NPE");
+                    }
                 }
             }
     }
@@ -21,6 +29,7 @@ public class Ex9 {
             case 0 : throw new MyExOne("1 :)");
             case 1 : throw new MyExTwo("2 :(");
             case 2 : throw new MyExThree("3 :[");
+            case 3 : throw new NullPointerException();
 
         }
     }

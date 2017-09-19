b05485f92533856a7802d811c848ca9bda0a4899
laa252159@gmail.com <Pyfybz!(85>
2017-09-19 16:41:56 +0400

    Ch12 Ex27

---
+++ src/gl12/hw/Ex27.java
@@ -0,0 +1,15 @@
+package gl12.hw;
+
+public class Ex27 {
+	public static void main(String[] args) {
+		int[] vals = { 1, 2 };
+		try {
+			System.out.println(vals[3]);
+			throw new MyException("dd");
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			System.out.println(ex);
+		} catch (MyException me) {
+			System.out.println(me.getStr());
+		}
+	}
+}
--- src/gl12/hw/Ex3.java
+++ src/gl12/hw/Ex3.java
@@ -4,12 +4,16 @@ public class Ex3 {
 	public static void main(String[] args) {
 		int[] vals = { 1, 2 };
 		try {
-			System.out.println(vals[3]);
-			throw new MyException("dd");
+			System.out.println(vals[1]);
+			throw new MyRuntimeException();
 		} catch (ArrayIndexOutOfBoundsException ex) {
 			System.out.println(ex);
-		} catch (MyException me) {
-			System.out.println(me.getStr());
+		} catch (MyRuntimeException me) {
+			System.out.println(me);
 		}
 	}
+}
+
+class MyRuntimeException extends RuntimeException {
+
 }

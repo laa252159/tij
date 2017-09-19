7a26b63f8695bafa29b6c9739c6dfc61c706a16b
Alexander Lobov <Aleksandr_Lobov@epam.com>
2017-09-19 20:36:14 +0400

    fix compilation on MAC

--- src/strings/InfiniteRecursionA.java
+++ src/strings/InfiniteRecursionA.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class InfiniteRecursionA {
   public String toString() {
-    return " InfiniteRecursion address: " + this + "\n";
+    return " InfiniteRecursion address: " + super.toString() + "\n";
   }
   public static void main(String[] args) {
     List<InfiniteRecursionA> v =

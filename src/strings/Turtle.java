26ea5dfddb6422f2fcdebe81f1586bca3a83880e
laa252159@gmail.com <Pyfybz!(85>
2017-09-20 16:45:29 +0400

    Ch13 Ex3

--- src/strings/Turtle.java
+++ src/strings/Turtle.java
@@ -9,17 +9,19 @@ public class Turtle {
     this.name = name;
     this.f = f;
   }
-  public void move(int x, int y) {
-    f.format("%s The Turtle is at (%d,%d)\n", name, x, y);
+  public void move(double x, double y) {
+    f.format("%s The Turtle is at (%.0f,%.0f)\n", name, x, y);
   }
   public static void main(String[] args) {
     PrintStream outAlias = System.out;
+    PrintStream err = System.err;
+
     Turtle tommy = new Turtle("Tommy",
       new Formatter(System.out));
     Turtle terry = new Turtle("Terry",
-      new Formatter(outAlias));
+      new Formatter(err));
     tommy.move(0,0);
-    terry.move(4,8);
+    terry.move(4.4,8);
     tommy.move(3,4);
     terry.move(2,5);
     tommy.move(3,3);

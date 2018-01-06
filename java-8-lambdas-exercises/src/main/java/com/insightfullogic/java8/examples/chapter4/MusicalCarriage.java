2ed71e4d477538b75714d4ba075479e130de27dc
Alexander Lobov <Aleksandr_Lobov@epam.com>
2018-01-06 22:14:25 +0400

    Lambdas

--- java-8-lambdas-exercises/src/main/java/com/insightfullogic/java8/examples/chapter4/MusicalCarriage.java
+++ java-8-lambdas-exercises/src/main/java/com/insightfullogic/java8/examples/chapter4/MusicalCarriage.java
@@ -4,8 +4,14 @@ package com.insightfullogic.java8.examples.chapter4;
 public class MusicalCarriage
         implements Carriage, Jukebox {
 
+    public static void main(String[] args) {
+        MusicalCarriage m = new MusicalCarriage();
+        m.rock();
+    }
+
     @Override
     public String rock() {
+        System.out.println(Carriage.super.rock() + " !!!");
         return Carriage.super.rock();
     }
 

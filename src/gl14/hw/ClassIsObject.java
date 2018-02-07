cfd6ddfcea5cd837e06e9e0ef9e29c8f9416133b
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-02-07 17:41:47 +0400

    Thinkink in Java Ch14 Ex10

---
+++ src/gl14/hw/ClassIsObject.java
@@ -0,0 +1,10 @@
+package gl14.hw;
+
+public class ClassIsObject {
+
+    public static void main(String[] args) {
+        char[] chars = {'a', 'b', 'c', 'd'};
+        System.out.println(chars instanceof Object);
+    }
+
+}

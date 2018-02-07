6c8d56f90dd84bc4872fc4375338c1d4adfe37ef
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-02-07 17:58:59 +0400

    Thinkink in Java Ch14 test

--- src/gl14/hw/ClassIsObject.java
+++ src/gl14/hw/ClassIsObject.java
@@ -7,6 +7,19 @@ public class ClassIsObject {
     public static void main(String[] args) {
         char[] chars = {'a', 'b', 'c', 'd'};
         int[] ints = {1, 2, 3, 4};
+
+        int i = 0;
+
+        System.out.println(" *** " + char[].class);
+        System.out.println(" *** " + long[].class);
+        System.out.println(" *** " + String[].class);
+        System.out.println(" *** " + float[].class);
+        System.out.println(" *** " + double[].class);
+        System.out.println(" *** " + boolean[].class);
+        System.out.println(" *** " + boolean[].class);
+
+        System.out.println(" *** " + int.class);
+
         System.out.println(chars.getClass());
         System.out.println(Arrays.toString(ints.getClass().getDeclaredFields()));
         System.out.println(chars instanceof Object);

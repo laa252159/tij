498be4dc1e6b8b49d19a535a433dfcb220fe0eaf
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-02-07 17:37:11 +0400

    Thinkink in Java Ch14 Ex9

--- src/gl14/hw/ClassIerarhy.java
+++ src/gl14/hw/ClassIerarhy.java
@@ -1,5 +1,11 @@
 package gl14.hw;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+import org.mockito.cglib.core.CollectionUtils;
+import org.mockito.internal.util.collections.ArrayUtils;
+
 interface FirstIface {
 }
 
@@ -8,9 +14,14 @@ interface SecondIface {
 
 
 class FirstClass implements FirstIface {
+    int a;
+    int b;
 }
 
 class SecondClass extends FirstClass {
+    String c;
+    String d;
+
 }
 
 class FirdClass extends SecondClass {
@@ -44,12 +55,23 @@ public class ClassIerarhy {
             sb.append(" : ");
         }
 
+        Class clSuperclass = cl.getSuperclass();
+        sb.append(" " + cl + "\n");
+
+        if (cl.getDeclaredFields() != null && cl.getDeclaredFields().length > 0) {
+            sb.append("fields: \n");
+            for (Field field : cl.getDeclaredFields()) {
+                sb.append(field.getName() + "   \n");
+            }
+        }
+
+//        sb.append("\n");
+
         for (Class ifaceClass : cl.getInterfaces()) {
             sb.append(" " + ifaceClass + " *** ");
         }
 
-        Class clSuperclass = cl.getSuperclass();
-        sb.append(" " + cl + "\n");
+
         if (clSuperclass != null) {
             returnAllAncestors(clSuperclass, sb);
         }

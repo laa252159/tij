dcfc5c67cd8ade70e6655b09a798b4937dc4ffcc
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-02-07 17:45:08 +0400

    Thinkink in Java Ch14 Ex10

--- src/gl14/hw/ClassIsObject.java
+++ src/gl14/hw/ClassIsObject.java
@@ -1,9 +1,14 @@
 package gl14.hw;
 
+import java.util.Arrays;
+
 public class ClassIsObject {
 
     public static void main(String[] args) {
         char[] chars = {'a', 'b', 'c', 'd'};
+        int[] ints = {1, 2, 3, 4};
+        System.out.println(chars.getClass());
+        System.out.println(Arrays.toString(ints.getClass().getDeclaredFields()));
         System.out.println(chars instanceof Object);
     }
 

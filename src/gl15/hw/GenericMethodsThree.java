6072aa9dae0111ebd2207208457d7c6ca5d5c505
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-02-22 16:58:18 +0400

    Thinkink in Java Ch15 Ex10

--- src/gl15/hw/GenericMethodsThree.java
+++ src/gl15/hw/GenericMethodsThree.java
@@ -1,13 +1,9 @@
-package gl15.hw;//: generics/GenericMethods.java
+package gl15.hw;
 
-import java.math.BigDecimal;
 import java.util.Random;
 
-import concurrency.NotifyVsNotifyAll;
-import gl4.hw.NotDivNums;
-
 public class GenericMethodsThree {
-    public <A, B, C> void f(A a, B b, C c) {
+    public <A, B> void f(A a, B b, Integer c) {
         System.out.print(a.getClass().getName() + "\t");
         System.out.print(b.getClass().getName() + "\t");
         System.out.print(c.getClass().getName() + "\t");
@@ -17,17 +13,10 @@ public class GenericMethodsThree {
     public static void main(String[] args) {
         GenericMethodsThree gm = new GenericMethodsThree();
         gm.f("", 1, 3);
-        gm.f(1, "", new Double(2));
-        gm.f(1.0, new Random(2), new BigDecimal(2));
-        gm.f(1.0F, "", new NotifyVsNotifyAll());
-        gm.f('c', "", new StringBuilder("f"));
-        gm.f(gm, 2, 001f);
+        gm.f(1, "", 2);
+        gm.f(1.0, new Random(2), 7);
+        gm.f(1.0F, "", 0);
+        gm.f('c', "", 1);
+        gm.f(gm, 2, 1);
     }
-} /* Output:
-java.lang.String
-java.lang.Integer
-java.lang.Double
-java.lang.Float
-java.lang.Character
-GenericMethods
-*///:~
+}

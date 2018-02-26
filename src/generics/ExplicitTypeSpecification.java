b4afd6188d60e3626cbdadaf7988685eb6c70de4
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-02-26 14:26:08 +0400

    Thinkink in Java Ch15 Ex12 Skipped!!!

--- src/generics/ExplicitTypeSpecification.java
+++ src/generics/ExplicitTypeSpecification.java
@@ -6,6 +6,6 @@ import net.mindview.util.*;
 public class ExplicitTypeSpecification {
   static void f(Map<Person, List<Pet>> petPeople) {}
   public static void main(String[] args) {
-    f(New.<Person, List<Pet>>map());
+    f(New.<Person, List<Pet>>map());//
   }
 } ///:~

10037785394cd681112ac3a2b41666ca9c590e33
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-03-05 16:16:09 +0400

    Thinkink in Java Ch15 refactoring

--- src/generics/CompilerIntelligence.java
+++ src/generics/CompilerIntelligence.java
@@ -1,12 +1,13 @@
 package generics;//: generics/CompilerIntelligence.java
+
 import java.util.*;
 
 public class CompilerIntelligence {
-  public static void main(String[] args) {
-    List<? extends Fruit> flist =
-      Arrays.asList(new Apple());
-    Apple a = (Apple)flist.get(0); // No warning
-    flist.contains(new Apple()); // Argument is 'Object'
-    flist.indexOf(new Apple()); // Argument is 'Object'
-  }
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        List<? extends Fruit> flist = Arrays.asList(new Fruit(), new Orange(), apple);
+        Apple a = (Apple) flist.get(2); // No warning
+        System.out.println("flist.contains(new Apple()) " + flist.contains(apple)); // Argument is 'Object'
+        System.out.println("flist.indexOf(new Apple()) " + flist.indexOf(apple)); // Argument is 'Object'
+    }
 } ///:~

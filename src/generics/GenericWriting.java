269f60e56ffb7bc0bf2f0324be1ff5770c606d73
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-03-06 11:39:19 +0400

    Thinkink in Java Ch15 refactoring

--- src/generics/GenericWriting.java
+++ src/generics/GenericWriting.java
@@ -1,24 +1,37 @@
 package generics;//: generics/GenericWriting.java
+
 import java.util.*;
 
 public class GenericWriting {
-  static <T> void writeExact(List<T> list, T item) {
-    list.add(item);
-  }
-  static List<Apple> apples = new ArrayList<Apple>();
-  static List<Fruit> fruit = new ArrayList<Fruit>();
-  static void f1() {
-    writeExact(apples, new Apple());
-    // writeExact(fruit, new Apple()); // Error:
-    // Incompatible types: found Fruit, required Apple
-  }
-  static <T> void
-  writeWithWildcard(List<? super T> list, T item) {
-    list.add(item);
-  }	
-  static void f2() {
-    writeWithWildcard(apples, new Apple());
-    writeWithWildcard(fruit, new Apple());
-  }
-  public static void main(String[] args) { f1(); f2(); }
+    static <T> void writeExact(List<T> list, T item) {
+        list.add(item);
+    }
+
+    static List<Apple> apples = new ArrayList<Apple>();
+    static List<Fruit> fruits = new ArrayList<Fruit>();
+
+    static void f1() {
+        writeExact(apples, new Apple());
+        // writeExact(fruits, new Apple()); // Error:
+        // Incompatible types: found Fruit, required Apple
+    }
+
+    static <T> void writeWithWildcard(List<? super T> list, T item) {
+        list.add(item);
+    }
+
+    static <T> void writeExactFruit(List<? super T> fruits, T fruit){
+        fruits.add(fruit);
+    }
+
+    static void f2() {
+        writeWithWildcard(apples, new Apple());
+        writeWithWildcard(fruits, new Apple());
+    }
+
+    public static void main(String[] args) {
+        f1();
+        f2();
+        writeExactFruit(fruits, new Apple());
+    }
 } ///:~

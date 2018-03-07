e53ee53458ebd8333a976834f4de3a6f658c88e2
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-03-07 12:51:20 +0400

    Thinkink in Java Ch15 refactoring

--- src/generics/MultipleInterfaceVariants.java
+++ src/generics/MultipleInterfaceVariants.java
@@ -1,8 +1,10 @@
 package generics;//: generics/MultipleInterfaceVariants.java
 // {CompileTimeError} (Won't compile)
 
-interface Payable<T> {}
+interface Payable<T> {
+}
 
-class Employee1 implements Payable<Employee1> {}
+class Employee1 implements Payable<Employee1> {
+}
 //class Hourly extends Employee1
 //  implements Payable<Hourly> {} ///:~

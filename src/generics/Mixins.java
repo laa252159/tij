dcfa08b9259d981bb9fbcde29449bf816eaf9555
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-03-13 16:58:13 +0400

    Thinkink in Java Ch15 Ex37

--- src/generics/Mixins.java
+++ src/generics/Mixins.java
@@ -49,17 +49,34 @@ class BasicImp implements Basic {
     }
 }
 
-class Mixin extends BasicImp implements TimeStamped, SerialNumbered {
+interface Coloreded {
+    String color();
+}
+
+class ColorodedImpl implements Coloreded {
+    final static String[] colors = {"RED", "WHITE", "YELLOW"};
+
+    @Override
+    public String color() {
+        return colors[new Random().nextInt(3)];
+    }
+}
+
+class Mixin extends BasicImp implements TimeStamped, SerialNumbered, Coloreded {
     private TimeStamped timeStamp = new TimeStampedImp();
     private SerialNumbered serialNumber =
             new SerialNumberedImp();
-
+    private ColorodedImpl ci = new ColorodedImpl();
     public long getStamp() {
         return timeStamp.getStamp();
     }
 
     public long getSerialNumber() {
         return serialNumber.getSerialNumber();
+    }
+
+    public String color() {
+        return ci.color();
     }
 }
 
@@ -69,9 +86,9 @@ public class Mixins {
         mixin1.set("test string 1");
         mixin2.set("test string 2");
         System.out.println(mixin1.get() + " " +
-                mixin1.getStamp() + " " + mixin1.getSerialNumber());
+                mixin1.getStamp() + " " + mixin1.getSerialNumber() + " " + mixin1.color());
         System.out.println(mixin2.get() + " " +
-                mixin2.getStamp() + " " + mixin2.getSerialNumber());
+                mixin2.getStamp() + " " + mixin2.getSerialNumber() + " " + mixin1.color());
     }
 } /* Output: (Sample)
 test string 1 1132437151359 1

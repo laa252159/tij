23416044b5a6de4c5faa9e284013c82a9d72554e
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-03-16 14:56:16 +0400

    Thinkink in Java Ch16 Ex5

---
+++ src/gl16/hw/Gl16Ex5.java
@@ -0,0 +1,9 @@
+package gl16.hw;
+
+import java.util.Arrays;
+
+public class Gl16Ex5 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(new Object[3][3][3]));
+    }
+}

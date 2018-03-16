bc659ce9c8a6a8dd4cc890d687b5416d69ed6195
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-03-16 16:06:56 +0400

    Thinkink in Java Ch16 refactoring

--- src/arrays/ArrayOfGenericType.java
+++ src/arrays/ArrayOfGenericType.java
@@ -1,13 +1,18 @@
 package arrays;//: arrays/ArrayOfGenericType.java
 // Arrays of generic types won't compile.
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayOfGenericType<T> {
-  T[] array; // OK
-  @SuppressWarnings("unchecked")
-  public ArrayOfGenericType(int size) {
-    //! array = new T[size]; // Illegal
-    array = (T[])new Object[size]; // "unchecked" Warning
-  }
-  // Illegal:
-  //! public <U> U[] makeArray() { return new U[10]; }
+    T[] array; // OK
+
+    @SuppressWarnings("unchecked")
+    public ArrayOfGenericType(int size) {
+        List<T> list = new ArrayList<T>();
+        //! array = new T[size]; // Illegal
+        array = (T[]) new Object[size]; // "unchecked" Warning
+    }
+    // Illegal:
+    //! public <U> U[] makeArray() { return new U[10]; }
 } ///:~

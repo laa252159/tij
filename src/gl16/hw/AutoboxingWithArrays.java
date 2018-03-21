7d59980c197ca9a215d577786aefb53d979efa4a
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-03-21 10:36:43 +0400

    Thinkink in Java Ch16 Ex11

---
+++ src/gl16/hw/AutoboxingWithArrays.java
@@ -0,0 +1,10 @@
+package gl16.hw;
+
+public class AutoboxingWithArrays {
+    public static void main(String[] args) {
+        int[] pa = { 1, 2, 3, 4, 5 };
+//!        Integer[] wa = pa;
+        Integer[] wb = { 1, 2, 3, 4, 5 };
+//!        int[] pb = wb;
+    }
+}

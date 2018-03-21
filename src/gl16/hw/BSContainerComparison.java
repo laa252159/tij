9e68fc3938837969c6bce6e2f98bff9108a2fa1c
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-03-21 10:52:57 +0400

    Thinkink in Java Ch16 Ex15

---
+++ src/gl16/hw/BSContainerComparison.java
@@ -0,0 +1,35 @@
+package gl16.hw;
+
+import java.util.*;
+
+import net.mindview.util.*;
+
+import static net.mindview.util.Print.*;
+
+public class BSContainerComparison {
+    public static void main(String[] args) {
+        BSGenerator gen = new BSGenerator();
+        BerylliumSphere[] spheres = Generated.array(
+                BerylliumSphere.class, gen, 5);
+        print(Arrays.toString(spheres));
+        print(spheres[4]);
+        List<BerylliumSphere> sphereList = Arrays.asList(
+                Generated.array(BerylliumSphere.class, gen, 5));
+        print(sphereList);
+        print(sphereList.get(4));
+        int[] integers = {0, 1, 2, 3, 4, 5};
+        print(Arrays.toString(integers));
+        print(integers[4]);
+        List<Integer> intList = new ArrayList<Integer>(
+                Arrays.asList(0, 1, 2, 3, 4, 5));
+        intList.add(97);
+        print(intList);
+        print(intList.get(4));
+    }
+}
+
+class BSGenerator implements Generator<BerylliumSphere> {
+    public BerylliumSphere next() {
+        return new BerylliumSphere();
+    }
+}

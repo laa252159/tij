e08e2c8293ecf9490d6140d515202cac8d3c5fa1
Aleksandr_Lobov <aleksandr_lobov@epam.com>
2018-04-05 11:27:54 +0400

    Thinkink in Java Ch16 Ex11

---
+++ src/gl17/hw/PriorityQueueTest.java
@@ -0,0 +1,28 @@
+package gl17.hw;
+
+import java.util.*;
+
+public class PriorityQueueTest {
+    public static void main(String[] args) {
+        PriorityQueue<Item> queue = new PriorityQueue<Item>();
+        for(int i = 0; i < 22; i++)
+            queue.add(new Item());
+        Item item;
+        while((item = queue.poll()) != null)
+            System.out.println(item);
+    }
+}
+
+class Item implements Comparable<Item> {
+    private static final Random rnd = new Random(47);
+    private Integer priority = rnd.nextInt(101);
+
+    public int compareTo(Item arg) {
+        return priority < arg.priority ? -1 :
+                priority == arg.priority ? 0 : 1;
+    }
+
+    public String toString() {
+        return Integer.toString(priority);
+    }
+}
